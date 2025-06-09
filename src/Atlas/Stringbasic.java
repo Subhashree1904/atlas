63e92bd4441fd52b2ea72befe620011e6153fe65
subh <subhnayak19@gmail.com>
2025-06-09 12:26:58 +0530

    changes in duplicate characters

--- src/Atlas/Stringbasic.java
+++ src/Atlas/Stringbasic.java
@@ -1,10 +1,23 @@
 package Atlas;
 
-public class Stringbasic {
+public class DuplicateCharacters {
 
 	public static void main(String[] args) {
 		
-
+		String str = "programming";
+		int[] count = new int[256];
+		
+	for(int i = 0; i<str.length(); i++) {
+		count[str.charAt(i)]++;
+	}
+	System.out.println("Duplicate characters on string");
+	
+	for(int i = 0; i<256; i++) {
+		if(count[i]>1) {
+			System.out.println((char) i + " " + count[i] + "times");
+		}
+	}
+	
 	}
 
 }
